1b24bb8d80669c8e1cdd951e451294ed3a4b1e48
Elliot Hume <elliot@hume.ca>
2017-06-16 11:57:48 -0400

    Add files via upload

--- WebEnigma/scripts.js
+++ WebEnigma/scripts.js
@@ -84,7 +84,6 @@ function action(keyword){
 				return "You may not skip this question.";
 			}
 		} else {
-			//console.log(decrypt(puzzle.alt_data));
 			return "Incorrect";
 		}
 	}
@@ -102,6 +101,35 @@ class Puzzle {
 	}
 
 	build(){
+		function decrypt(message){
+			var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+			var U_alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
+			var keys = "qazwsxedcrfvtgbyhnujmikolp".split("");
+			var U_keys = "qazwsxedcrfvtgbyhnujmikolp".toUpperCase().split("");
+
+
+			var keydict = {};
+			for(var i=0; i < alphabet.length; i++){
+				keydict[keys[i]] = alphabet[i];
+			}
+
+			var U_keydict = {};
+			for(var i=0; i < U_alphabet.length; i++){
+				U_keydict[U_keys[i]] = U_alphabet[i];
+			}
+
+			var loc = message.split("");
+			for (var i=0; i < loc.length; i++){
+				if($.inArray(loc[i], alphabet) != -1 ){
+					if(loc[i] == loc[i].toLowerCase()){
+						loc[i] = keydict[loc[i]];
+					} else {
+						loc[i] = U_keydict[loc[i]];
+					}
+				}
+			}
+			return loc.join("");
+		}
 		this._data = decrypt("    Fcnuj Qmsujcbg~Wdb qt I?~ymppvsu/ctqesu/shmqjcbg.ryse~jnms~xcabgqzzc*    Rcwwvs~Au q ujbgs cgucws q jnss, I'vv dsvy lbmn kbnwu bmjvcis jdss._Bmj cx lbm ymud ts qu I ujqgw, jds tbns I tbis jds vsuu I qt._ _Wdqj qt I?~gmvv~~q ysgzcv_ysgzcv*    Ayjcjmws jsuj~ Wdqj cu jds gsoj gmtasn cg jdcu ushmsgzs?_0 1 3 3 8 21 ?~gmvv~jnms~165*    Rcwwvs~Wdqj cu asjjsn jdqg ebw, kbnus jdqg jds wsicv,_wsqw ysbyvs sqj cj, amj cx lbm sqj cj lbm wcs?~gmvv~~gbjdcge*    ????~bwebibn rs rswgbujqiqg~gmvv~jnms~uctyvs*    Psnubgqv Rcwwvs (Equl)~_A yqvs udqwbk bg wqnf kqjsn,_Ybm'vv uss ts kdsg ucedj cu vbuj,_A dqvx bx ts cu qvv lbm'vv uss qgw sisg jdqj tql wcxxsn._ _Wdqj qt I?~gmvv~~jds tbbg_tbbg*    Psnubgqv Rcwwvs (Dcxxczmvj)~_I qt ecisg cg jctsu kdsg q emcwcge dqgw cu gsswsw,_Ybm kcvv gbj gssw ts cx lbm dqis ts,_amj kdsg lbm gssw ts, kcjd q ujnqgesn I kcvv as._ _Wdqj qt I?~~gmvv~wcnszjcbgu_q wcnszjcbg_wcnszjcbg*    Shmqnsu~Hbk tqgl uhmqnsu qns jdsns cg jdcu yczjmns?~ymppvsu/ctqesu/uhmqnsu.ryse~~40_41*    Wbnwu~Hbk tqgl tsqgcgeu wbsu jds kbnw \"Ogs\" dqis?~gmvv~jnms~3*    Wbnwu~Hbk tqgl tsqgcgeu wbsu jds kbnw \"Tdnss\" dqis?~gmvv~jnms~bgs*    Rcwwvs~Wdqj zqg bgs gbj dbvw, xsk fssy, qgw tqgl ansqf?~gmvv~~q usznsj_usznsju_usznsj*    Bnqcg jsqusn~Wdczd gmtasnu qns bg jds fslabqnw?~~gmvv~qvv bx jdst_qvv_cgxcgcjs_cgxcgcjl*    Tnczf~Wdqj wbsu bgs wb kdsg jdsl qns zbtcge cgjb q nbbt, bn zbtcge aqzf jb q nbbt~gmvv~~*    Qmsujcbg ncwwvs~Wdqj enbku kdsg cj sqju, amj wcsu kdsg cj wncgfu?~gmvv~~q xcns_xcns_q xvqts_xvqtsu_jssgqesnu*    Rcwwvs~I dqis q dsqw, q jqcv, qgw gbjdcge tbns._Wdqj qt I?~gmvv~~q zbcg_zbcg_zbcgu*    Wdqj qt I?~051409071301~gmvv~~sgcetq*    Bnqcg jsqusn~Sbts tbgjdu dqis 30 wqlu, ubts dqis 31 wqlu._Hbk tqgl tbgjdu dqis 28 wqlu?~gmvv~~12_qvv_qvv bx jdst*    Rcwwvs~Mqgl dqis dsqnw ts,_amj gbabwl dqu ussg ts,_qgw I kcvv gbj uysqf aqzf mgjcv uybfsg jb._Wdqj qt I?~gmvv~~qg szdb_szdb_szdbsu*    Sjbnl~THE Tncas vsqwsn Iegcjsw dcu sgstl'u zvbjdsu qu dcu Tncas Lcujsgsw jb jds ubmgw_bx jdscn yncubgEn'u yvsqwcge, kcjdbmj tsnzl bn nstbnus._Ig jdcu jqvs kcvv lbm xcgw jds qguksn~gmvv~~ujbnl*    Hcujbnl~Wdqj cu jds 6jd gqts ecisg jb jds Lqwl bx jds Lqfs?~gmvv~jnms~gcicqg*    Rcwwvs~A gqjmnqv ujqjs, I't ubmedj al qvv._Gb kcjdbmj ts, qgw lbm udqvv xqvv._Ybm wb ts kdsg lbm uysgw,_qgw mus ts kdsg lbm sqj jb gb sgw._Wdqj qt I?~gmvv~~aqvqgzs*    Vcanqjcge ybvsqnt~Wdqj cu ds jdqj amcvwu ujnbgesn jdqg scjdsn jds tqubg, jds udcykncedj, bn jds zqnysgjsn?~gmvv~jnms~jds enqiswceesn_q enqiswceesn_enqiswceesn_q enqis wceesn_jds enqis wceesn_enqis wceesn*    Rcwwvs~Wdb tqfsu cj wbsu gbj kqgj cj,_kdb amlu cj wbsu gbj gssw cj,_qgw kdb gsswu cj wbsu gbj fgbk cj_Wdqj cu cj?~gmvv~~q zbxxcg_zbxxcg_zbxxcgu*    Psnubgqv Rcwwvs~I dqis tqgl xqzsu qgw tqgl xcemnsu,_ub lbm kcvv gsisn emsuu kdqj tl zbvvszjcbg mgzbisnu._Wdcvs nsemvqn I tql as, I zqg nba lbm kcjd evss_xbn tl jnms ujnsgejd vcsu cg jds eqtavs_Wdqj qt c?~gmvv~~zqnwu_q zqnw_zqnw_q wszf bx zqnwu_wszf bx zqnwu*    Fcgqv hmsujcbg~Tdcu jdcge qvv jdcgeu wsibmnu:_Bcnwu, asquju, jnssu, xvbksnu;_Ggqku cnbg, acjsu ujssv;_Gncgwu dqnw ujbgsu jb tsqv;_Svqlu fcge, nmcgu jbkg,_Agw asqju dced tbmgjqcg wbkg._ _Wdqj qt I?~gmvv~~jcts*    ");
 		this.lines = this._data.split("*");
 		this.contentsList = this.lines[this.sequence].split("~");
@@ -157,36 +185,6 @@ function skipPuzzle(bol) {
 
 function titleClick(){
 	$("#input-box").val("Enigma");
-}
-
-function decrypt(message){
-	alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-	U_alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
-    keys = "qazwsxedcrfvtgbyhnujmikolp".split("");
-	U_keys = "qazwsxedcrfvtgbyhnujmikolp".toUpperCase().split("");
-
-
-    keydict = {};
-    for(var i=0; i < alphabet.length; i++){
-		keydict[keys[i]] = alphabet[i];
-	}
-
-    U_keydict = {};
-    for(var i=0; i < U_alphabet.length; i++){
-		U_keydict[U_keys[i]] = U_alphabet[i];
-	}
-
-    loc = message.split("");
-    for (var i=0; i < loc.length; i++){
-		if($.inArray(loc[i], alphabet) != -1 ){
-			if(loc[i] == loc[i].toLowerCase()){
-				loc[i] = keydict[loc[i]];
-			} else {
-				loc[i] = U_keydict[loc[i]];
-			}
-		}
-	}
-    return loc.join("");
 }
 
 function encrypt(message){
