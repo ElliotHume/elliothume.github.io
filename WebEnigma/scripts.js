4edd2745f0cc700b69af59ab16efdb91ca5603d6
Elliot Hume <elliot@hume.ca>
2017-06-21 16:51:23 -0400

    Add files via upload

--- WebEnigma/scripts.js
+++ WebEnigma/scripts.js
@@ -3,6 +3,7 @@ var sequence = 0;
 var puzzle = null;
 var skips = 0;
 var turing_bonus = false;
+var sequence_length = 30;
 
 $(document).ready(function(){
 	var $Q = $('.question-box');
@@ -18,7 +19,7 @@ $(document).ready(function(){
     $(document).keydown(function(key){
         vale = $c.val().toLowerCase();
 		if (key.which == 13){
-			if (vale.length > 0 || sequence==12){
+			if (vale.length > 0 || puzzle.keys[0]==""){
 				$P.prepend('<p>> '+vale+': '+ action(vale)+'</p>');
 				$c.val("");
 			}
@@ -55,7 +56,7 @@ function action(keyword){
 	if (!active){
         if (keyword == "start"){
     		active = true;
-			$("h1").css("width", "5vw");
+			$("h1").css("width", "0");
 			$('.skip-button').css("display","inline");
 			$('.button').css("display","inline");
 			$('#command').css('color', 'rgb(5,200,5)');
@@ -63,13 +64,13 @@ function action(keyword){
 			$('#title-bar').attr("onclick", "#");
 			$("#google-icon").css("display", "inline");
 			puzzle.display()
-    		return "let the games begin, you may skip up to 3 puzzles with the keyword 'skip' or the button.";
+    		return "let the games begin, you may skip up to 5 puzzles with the keyword 'skip' or the button.";
         } else if (keyword == "enigma"){
             return "What do you need to do to finish a race?";
         }
     } else {
 		if (puzzle.keys.indexOf(encrypt(keyword)) != -1){
-			if (sequence < 24){
+			if (sequence < sequence_length){
 				sequence++;
 				puzzle = new Puzzle(sequence);
 				puzzle.display()
@@ -80,11 +81,19 @@ function action(keyword){
 			$("#puzzle_name").css("width", "100%");
 			$("#puzzle_text").html("");
 			$("#puzzle_image").attr("src", "");
-			if (skips == 0){
+			if (skips == 0 || skips == -1){
 				$("#puzzle_name").css("color","rgb(5,200,5)");
 				$('h1').css('color', 'grey');
 				$("#puzzle_name").html("You have solved the enigma");
 				return "You have solved the enigma";
+			} else if (skips < -1){
+				$("#puzzle_name").css("color","darkred");
+				$("#title-bar").css("outline-color","darkred");
+				$('h1').css('color', 'grey');
+				$('h1').css('text-shadow', '2px 2px darkgrey');
+				$('#title-bar').css('border-image', 'linear-gradient(to bottom, darkred, black) 1 100%');
+				$("#puzzle_name").html("You have been identified as a cheater. Your IP address has been recorded.");
+				return "You have been identified as a cheater. Your IP address has been recorded.";
 			} else {
 				$("#puzzle_name").css("color","darkred");
 				$("#title-bar").css("outline-color","darkred");
@@ -96,7 +105,7 @@ function action(keyword){
 			}
 			
 		} else if (keyword == "skip"){
-			if (sequence < 24){
+			if (sequence < sequence_length){
 				return skipPuzzle(false);
 			} else {
 				return "You may not skip this question.";
@@ -148,8 +157,9 @@ class Puzzle {
 			}
 			return loc.join("");
 		}
-		this._data = decrypt("    Fcnuj Qmsujcbg~Wdb qt I?~ymppvsu/ctqesu/shmqjcbg.ryse~jnms~xcabgqzzc*    Rcwwvs~Au q ujbgs cgucws q jnss, I'vv dsvy lbmn kbnwu bmjvcis jdss._Bmj cx lbm ymud ts qu I ujqgw, jds tbns I tbis jds vsuu I qt._ _Wdqj qt I?~gmvv~~q ysgzcv_ysgzcv*    Ayjcjmws jsuj~ Wdqj cu jds gsoj gmtasn cg jdcu ushmsgzs?_0 1 3 3 8 21 ?~gmvv~jnms~165*    Rcwwvs~Wdqj cu asjjsn jdqg ebw, kbnus jdqg jds wsicv,_wsqw ysbyvs sqj cj, amj cx lbm sqj cj lbm wcs?~gmvv~~gbjdcge*    ????~bwebibn rs rswgbujqiqg~gmvv~jnms~uctyvs*    Psnubgqv Rcwwvs (Equl)~_A yqvs udqwbk bg wqnf kqjsn,_Ybm'vv uss ts kdsg ucedj cu vbuj,_A dqvx bx ts cu qvv lbm'vv uss qgw sisg jdqj tql wcxxsn._ _Wdqj qt I?~gmvv~~jds tbbg_tbbg*    Psnubgqv Rcwwvs (Dcxxczmvj)~_I qt ecisg cg jctsu kdsg q emcwcge dqgw cu gsswsw,_Ybm kcvv gbj gssw ts cx lbm dqis ts,_amj kdsg lbm gssw ts, kcjd q ujnqgesn I kcvv as._ _Wdqj qt I?~~gmvv~wcnszjcbgu_q wcnszjcbg_wcnszjcbg*    Shmqnsu~Hbk tqgl uhmqnsu qns jdsns cg jdcu yczjmns?~ymppvsu/ctqesu/uhmqnsu.ryse~~40_41*    Wbnwu~Hbk tqgl tsqgcgeu wbsu jds kbnw \"Ogs\" dqis?~gmvv~jnms~3*    Wbnwu~Hbk tqgl tsqgcgeu wbsu jds kbnw \"Tdnss\" dqis?~gmvv~jnms~bgs*    Rcwwvs~Wdqj zqg bgs gbj dbvw, xsk fssy, qgw tqgl ansqf?~gmvv~~q usznsj_usznsju_usznsj*    Bnqcg jsqusn~Wdczd gmtasnu qns bg jds fslabqnw?~~gmvv~qvv bx jdst_qvv_cgxcgcjs_cgxcgcjl*    Tnczf~Wdqj wbsu bgs wb kdsg jdsl qns zbtcge cgjb q nbbt, bn zbtcge aqzf jb q nbbt~gmvv~~*    Qmsujcbg ncwwvs~Wdqj enbku kdsg cj sqju, amj wcsu kdsg cj wncgfu?~gmvv~~q xcns_xcns_q xvqts_xvqtsu_jssgqesnu*    Rcwwvs~I dqis q dsqw, q jqcv, qgw gbjdcge tbns._Wdqj qt I?~gmvv~~q zbcg_zbcg_zbcgu*    Wdqj qt I?~051409071301~gmvv~~sgcetq*    Bnqcg jsqusn~Sbts tbgjdu dqis 30 wqlu, ubts dqis 31 wqlu._Hbk tqgl tbgjdu dqis 28 wqlu?~gmvv~~12_qvv_qvv bx jdst*    Rcwwvs~Mqgl dqis dsqnw ts,_amj gbabwl dqu ussg ts,_qgw I kcvv gbj uysqf aqzf mgjcv uybfsg jb._Wdqj qt I?~gmvv~~qg szdb_szdb_szdbsu*    Sjbnl~THE Tncas vsqwsn Iegcjsw dcu sgstl'u zvbjdsu qu dcu Tncas Lcujsgsw jb jds ubmgw_bx jdscn yncubgEn'u yvsqwcge, kcjdbmj tsnzl bn nstbnus._Ig jdcu jqvs kcvv lbm xcgw jds qguksn~gmvv~~ujbnl*    Hcujbnl~Wdqj cu jds 6jd gqts ecisg jb jds Lqwl bx jds Lqfs?~gmvv~jnms~gcicqg*    Rcwwvs~A gqjmnqv ujqjs, I't ubmedj al qvv._Gb kcjdbmj ts, qgw lbm udqvv xqvv._Ybm wb ts kdsg lbm uysgw,_qgw mus ts kdsg lbm sqj jb gb sgw._Wdqj qt I?~gmvv~~aqvqgzs*    Vcanqjcge ybvsqnt~Wdqj cu ds jdqj amcvwu ujnbgesn jdqg scjdsn jds tqubg, jds udcykncedj, bn jds zqnysgjsn?~gmvv~jnms~jds enqiswceesn_q enqiswceesn_enqiswceesn_q enqis wceesn_jds enqis wceesn_enqis wceesn*    Rcwwvs~Wdb tqfsu cj wbsu gbj kqgj cj,_kdb amlu cj wbsu gbj gssw cj,_qgw kdb gsswu cj wbsu gbj fgbk cj_Wdqj cu cj?~gmvv~~q zbxxcg_zbxxcg_zbxxcgu*    Psnubgqv Rcwwvs~I dqis tqgl xqzsu qgw tqgl xcemnsu,_ub lbm kcvv gsisn emsuu kdqj tl zbvvszjcbg mgzbisnu._Wdcvs nsemvqn I tql as, I zqg nba lbm kcjd evss_xbn tl jnms ujnsgejd vcsu cg jds eqtavs_Wdqj qt c?~gmvv~~zqnwu_q zqnw_zqnw_q wszf bx zqnwu_wszf bx zqnwu*    Fcgqv hmsujcbg~Tdcu jdcge qvv jdcgeu wsibmnu:_Bcnwu, asquju, jnssu, xvbksnu;_Ggqku cnbg, acjsu ujssv;_Gncgwu dqnw ujbgsu jb tsqv;_Svqlu fcge, nmcgu jbkg,_Agw asqju dced tbmgjqcg wbkg._ _Wdqj qt I?~gmvv~~jcts*    ");
+		this._data = decrypt("    Fcnuj Qmsujcbg~Wdb qt I?~ymppvsu/ctqesu/shmqjcbg.ryse~jnms~xcabgqzzc*    Rcwwvs~Au q ujbgs cgucws q jnss, I'vv dsvy lbmn kbnwu bmjvcis jdss._Bmj cx lbm ymud ts qu I ujqgw, jds tbns I tbis jds vsuu I qt._ _Wdqj qt I?~gmvv~~q ysgzcv_ysgzcv*    Ayjcjmws jsuj~ Wdqj cu jds gsoj gmtasn cg jdcu ushmsgzs?_0 1 3 3 8 21 ?~gmvv~jnms~165*    Rcwwvs~Wdqj cu asjjsn jdqg ebw, kbnus jdqg jds wsicv,_wsqw ysbyvs sqj cj, amj cx lbm sqj cj lbm wcs?~gmvv~~gbjdcge*     ~bwebibn rs rswgbujqiqg~gmvv~jnms~uctyvs*    Psnubgqv Dnsqt Rcwwvs (Equl)~_A yqvs udqwbk bg wqnf kqjsn,_Ybm'vv uss ts kdsg ucedj cu vbuj,_A dqvx bx ts cu qvv lbm'vv uss qgw sisg jdqj tql wcxxsn._ _Wdqj qt I?~gmvv~~jds tbbg_tbbg*    Psnubgqv Rcwwvs (Vsnl Dcxxczmvj)~_I qt ecisg cg jctsu kdsg q emcwcge dqgw cu gsswsw,_Ybm kcvv gbj gssw ts cx lbm dqis ts,_amj kdsg lbm gssw ts, kcjd q ujnqgesn I kcvv as._ _Wdqj qt I?~~gmvv~wcnszjcbgu_q wcnszjcbg_wcnszjcbg*    Shmqnsu~Hbk tqgl uhmqnsu qns jdsns cg jdcu yczjmns?~ymppvsu/ctqesu/uhmqnsu.ryse~~40_41*    Psnubgqv Rcwwvs (Dcxxczmvj)~I qt jds ensqj bgs-slsw asquj jdqj wsujnblu qvv cg tl yqjd,_A wstbg kcjd gb xbnt amj q abwl bx knqjd,_I qt abng al jds svstsgju qgw zbts xnbt jds usq,_Tdnbmed gqjmns I tqfs zdqbu jdqj uynsqwu qznbuu vsqemsu._Wdqj qt I?~gmvv~jnms~dmnnczqgs_q dmnnczqgs_zlzvbgs_q zlzvbgs_jlydbbg_q jlydbbg_dmnnczqgsu_zlzvbgsu_jlydbbgu*    Wbnwu~Hbk tqgl tsqgcgeu wbsu jds kbnw \"Ogs\" dqis?~gmvv~xqvus~3*    Wbnwu~Hbk tqgl tsqgcgeu wbsu jds kbnw \"Tdnss\" dqis?~gmvv~jnms~bgs*    Rcwwvs~Kssy ts jb lbmnusvx qgw I kcvv as dsvw, udqns ts kcjd xsk qgw I zqg as fsyj, ecis ts jb tqgl qgw I kcvv as anbfsg_Wdqj qt I?~gmvv~~q usznsj_usznsju_usznsj*    Bnqcg jsqusn~Wdczd gmtasnu qns bg jds fslabqnw?~~gmvv~qvv bx jdst_qvv_cgxcgcjs_cgxcgcjl_sisnl bgs_sisnl gmtasn_qvv gmtasnu_qvv bx jds gmtasnu*    Tnczf~Wdqj wbsu bgs wb kdsg jdsl qns zbtcge cgjb q nbbt, bn zbtcge aqzf jb q nbbt~gmvv~~*    Rcwwvs~Wdqj enbku kdsg cj sqju, amj wcsu kdsg cj wncgfu?~gmvv~~q xcns_xcns_q xvqts_xvqtsu_jssgqesnu*    Rcwwvs~I dqis q dsqw, q jqcv, qgw gbjdcge tbns._Wdqj qt I?~gmvv~~q zbcg_zbcg_zbcgu*    Wdqj qt I?~051409071301~gmvv~~sgcetq*    Psnubgqv Rcwwvs (Dcxxczmvj)~Sbts uql lbm'vv uss cj ubbg, bjdsnu uql cj kcvv gsisn zbts._Avv kcvv uysqf bx cj, kcjd dbysu bn cg xsqn._Ij kcvv fcvv sisnl ascge qgw ancge sisnl zdcvw jb vcxs._Ij kcvv qvkqlu socuj qgw gsisn as jbmzdsw, xbn cj'u gqjmns cu tlujsnl qgw cj'u ybksn cu tmzd._Wdqj cu cj?~gmvv~jnms~jbtbnnbk_jds gsoj wql_jds xmjmns_xmjmns*    Gsbtsjnl~Wdczd jncqgevs kcjd jds ecisg ucws vsgejdu dqu q utqvvsn umnxqzs?_A: 300, 400, 700_B: 100, 200, 300~gmvv~jnms~gscjdsn_abjd_q qgw a_gbgs_qa*    Bnqcg jsqusn~Sbts tbgjdu dqis 30 wqlu, ubts dqis 31 wqlu._Hbk tqgl tbgjdu dqis 28 wqlu?~gmvv~~12_qvv_qvv bx jdst_sisnl bgs_sisnl tbgjd_qvv tbgjdu_qvv bx jds tbgjdu*    Rcwwvs~Mqgl dqis dsqnw ts,_amj gbabwl dqu ussg ts,_qgw I kcvv gbj uysqf aqzf mgjcv uybfsg jb._Wdqj qt I?~gmvv~~qg szdb_szdb_szdbsu*    Sjbnl~THE Tncas vsqwsn Iegcjsw dcu sgstl'u zvbjdsu qu dcu Tncas Lcujsgsw jb jds ubmgw_bx jdscn yncubgEn'u yvsqwcge, kcjdbmj tsnzl, kcjdbmj nstbnus._Ig jdcu jqvs kcvv lbm xcgw jds qguksn.~gmvv~~ujbnl*    Tnczfl Hcujbnl~Wdqj cu jds 6jd gqts ecisg jb jds Lqwl bx jds Lqfs?~gmvv~jnms~gcicqg*    Psnubgqv Rcwwvs (Mswcmt)~I dqis slsu amj wb gbj uss,_A tbmjd amj zqg gbj uysqf,_Hqgwu jdqj kcvv gbj dbvw,_Lseu jdqj kcvv gbj nmg,_Agw q dsqnj jdqj zqg gbj xssv._Wdqj qt I?~gmvv~xqvus~q zbnyus_zbnyus_q wsqw tqg_q wsqw ysnubg_jds wsqw_q wsqw abwl_wsqw abwl*    Dssy jdbmedju~101010_kdqj qt I?~gmvv~jnms~42_jds qguksn jb vcxs, jds mgcisnus, qgw sisnljdcge_jds qguksn jb jds mvjctqjs hmsujcbg bx vcxs, jds mgcisnus, qgw sisnljdcge*    Rcwwvs~A gqjmnqv ujqjs, I't ubmedj al qvv._Gb kcjdbmj ts, qgw lbm udqvv xqvv._Ybm wb ts kdsg lbm uysgw,_qgw mus ts kdsg lbm sqj jb gb sgw._Wdqj qt I?~gmvv~~aqvqgzs*    Vcanqjcge ybvsqnt~Wdqj cu ds jdqj amcvwu ujnbgesn jdqg scjdsn jds tqubg, jds udcykncedj, bn jds zqnysgjsn?~gmvv~jnms~jds enqiswceesn_q enqiswceesn_enqiswceesn_q enqis wceesn_jds enqis wceesn_enqis wceesn*    Rcwwvs~Wdb tqfsu cj wbsu gbj kqgj cj,_kdb amlu cj wbsu gbj gssw cj,_qgw kdb gsswu cj wbsu gbj fgbk cj._Wdqj cu cj?~gmvv~~q zbxxcg_zbxxcg_zbxxcgu*    Psnubgqv Rcwwvs (Vsnl Equl)~I dqis tqgl xqzsu qgw tqgl xcemnsu,_ub lbm kcvv gsisn emsuu kdqj tl zbvvszjcbg mgzbisnu._Wdcvs nsemvqn I tql as, I zqg nba lbm kcjd evss_xbn tl jnms ujnsgejd vcsu cg jds eqtavs_Wdqj qt c?~gmvv~~zqnwu_q zqnw_zqnw_q wszf bx zqnwu_wszf bx zqnwu*    Fcgqv hmsujcbg~Tdcu jdcge qvv jdcgeu wsibmnu:_Bcnwu, asquju, jnssu, xvbksnu;_Ggqku cnbg, acjsu ujssv;_Gncgwu dqnw ujbgsu jb tsqv;_Svqlu fcge, nmcgu jbkg,_Agw asqju dced tbmgjqcg wbkg._ _Wdqj qu cj?~gmvv~~jcts");
 		this.lines = this._data.split("*");
+		sequence_length = this.lines.length - 1;
 		this.contentsList = this.lines[this.sequence].split("~");
 		this.name = this.contentsList[0];
 		this.text = this.contentsList[1].split("_");
@@ -176,9 +186,9 @@ class Puzzle {
 }
 
 function skipPuzzle(bol) {
-	if (skips < 3) {
+	if (skips < 8) {
 		if(bol){
-			if (sequence < 24){
+			if (sequence < sequence_length){
 				skips++;
 				sequence++;
 				$('.previous-commands').prepend('<p>> skip: Skipped </p>');
@@ -211,7 +221,7 @@ function ruleClick(){
 	$('.previous-commands').prepend('<p>> 5: At any time you may restart by pressing the restart button in the top right corner, there is no limit to the amount of attempts you have. </p>');
 	$('.previous-commands').prepend('<p>> 4: Do not cheat. This game uses an honor system. I am aware that there are ways to cheat and I ask that you would please not use them. I use log files and will be able to see if/when your victory is legitimate. </p>');
 	$('.previous-commands').prepend('<p>> 3: You may use google for questions that have a green check mark in the top left corner, you may not if there is a red x. </p>');
-	$('.previous-commands').prepend('<p>> 2: You may skip up to 3 questions by using the keyword "skip" or by pressing the skip button in the top right corner </p>');
+	$('.previous-commands').prepend('<p>> 2: You may skip up to 8 questions by using the keyword "skip" or by pressing the skip button in the top right corner </p>');
 	$('.previous-commands').prepend('<p>> 1: Answer the puzzles by typing in the command box and pressing enter to submit. </p>');
 }
 
